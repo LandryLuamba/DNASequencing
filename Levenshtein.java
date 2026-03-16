0392669d5c0e72011d33b7f823b97b87b465e90a
LandryLuamba <146769561+LandryLuamba@users.noreply.github.com>
2026-03-16 13:16:50 -0600

    changes

--- Levenshtein.java
+++ Levenshtein.java
@@ -1,4 +1,22 @@
+/**
+ * A simple example similarity algorithm.
+ *
+ * This algorithm counts the number of matching characters
+ * in corresponding positions up to the length of the shorter sequence.
+ *
+ * @author Landry Vewenda
+ * @version 2026-03-11
+*/
 public class Levenshtein implements SequenceScoringAlgorithm {
+    /**
+     * Counts matching positions between two DNA sequences up to the
+     * length of the shorter sequence.
+     *
+     * @param databaseSequence a DNA sequence from the database
+     * @param querySequence the query DNA sequence
+     * @return the number of matching positions
+     */
+    @Override
     public double score(String databaseSequence, String querySequence) {
         int m = databaseSequence.length();
         int n = querySequence.length();
@@ -11,17 +29,23 @@ public class Levenshtein implements SequenceScoringAlgorithm {
             Math.min(score(databaseSequence, querySequence.substring(0, n - 1)),
             score(databaseSequence.substring(0, m - 1), querySequence)));
     }
-
+    
+    /**
+     * Returns the display name of this algorithm.
+     * @return the algorithm name
+     */
     @Override
     public String getName(){
         return "Levenshtein match";
     }
 
+    /**
+     * Indicate that larger scores are better matches.
+     * @return false because less matching positions means a better match
+     */
     @Override
     public boolean higherScoreIsBetter() {
         return false;
     }
-
-
 
 }
